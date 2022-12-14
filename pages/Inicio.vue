c6a297b186ba500a99ea45e2656954fbc7698db8
caio ramalho <caiorp.cf@hotmail.com>
2022-12-13 23:54:21 -0300

    .

--- pages/Inicio.vue
+++ pages/Inicio.vue
@@ -6,9 +6,19 @@ onMounted(() => {
 <template>
 
 <section class="bg-img">
-    <div class="md:container">
-        
-        <h1 class="text-white">INOVANDO A GESTÃO PÚBLICA COM TRANSPARÊNCIA E COMPETÊNCIA</h1>
+    <div class="lg:container mx-auto my-auto py-4">        
+        <div class="flex">
+            <div class="max-w-screen-sm">
+
+                <h1 class="break-all font-semibold text-white text-2xl">
+                    INOVANDO A GESTÃO PÚBLICA COM TRANSPARÊNCIA E COMPETÊNCIA</h1>
+            </div>
+            <div class="mx-8">
+                <button type="button" class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                    Entrar em Contato
+                </button>
+            </div>
+        </div>
     </div>
 </section>
 </template>
