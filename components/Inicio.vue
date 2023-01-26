12f105a81c5c8450725a413ee94fb30b25cc12e1
Caio <caiorp.cf@hotmail.com>
2023-01-25 22:29:03 -0300

    card

--- components/Inicio.vue
+++ components/Inicio.vue
@@ -6,6 +6,21 @@ useHead({
 onMounted(() => {
     
 });
+
+const itensCard = [
+  {
+    titulo: 'Transparência',
+    texto: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus consectetur ratione quisquam veniam, amet harum corporis placeat tempore, aliquam culpa molestiae laboriosam, impedit fugit natus dolores iusto. Dicta, reiciendis totam.'
+  },
+  {
+    titulo: 'Gestão',
+    texto: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus consectetur ratione quisquam veniam, amet harum corporis placeat tempore, aliquam culpa molestiae laboriosam, impedit fugit natus dolores iusto. Dicta, reiciendis totam.'
+  },
+  {
+    titulo: 'Prestação',
+    texto: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus consectetur ratione quisquam veniam, amet harum corporis placeat tempore, aliquam culpa molestiae laboriosam, impedit fugit natus dolores iusto. Dicta, reiciendis totam.'
+  }
+]
 </script>
 <template>
 <section>
@@ -15,7 +30,7 @@ onMounted(() => {
       <div class="mx-auto max-w-3xl pt-20 pb-32 sm:pt-48 sm:pb-40">
         <div>
           <div>
-            <h1 class="text-4xl font-bold tracking-tight sm:text-center sm:text-6xl">Inovando a Gestão Pública com Transparência Competência</h1>
+            <h1 class="text-4xl font-bold tracking-tight sm:text-center sm:text-6xl">Inovando a Gestão Pública com Transparência e Competência</h1>
             <p class="mt-6 text-lg leading-8 text-gray-600 sm:text-center">Mais de 20 anos de experiência no desenvolvimento de soluções de Software para esfera pública e privada.
 
             </p>
@@ -25,12 +40,12 @@ onMounted(() => {
                 <span class="text-indigo-200" aria-hidden="true">&rarr;</span>
               </a>
               <a href="#" class="inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-gray-900 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                Visualizar Produtos
+                Visualizar Soluções
                 <span class="text-gray-400" aria-hidden="true">&rarr;</span>
               </a>
             </div>
           </div>
-          <!-- <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
+          <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
             <svg class="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]" viewBox="0 0 1155 678" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)" fill-opacity=".3" d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z" />
               <defs>
@@ -40,39 +55,33 @@ onMounted(() => {
                 </linearGradient>
               </defs>
             </svg>
-          </div> -->
+          </div>
         </div>
       </div>
     </div>
   </main>
 </div>
 </section>
-<div class="flex gap-2 py-6 px-36">
-    <div class="grid grid-cols-2 gap-2">
-      <div>
-        <img class="" src="/pessoas.jpg" alt="pessoas">
-      </div>
-      <div class="px-5 border-slate-300 border rounded-md w-5/12">
+<div class="flex flex-row gap-28 py-6 px-24 justify-center">
+      <div class="px-5 border-slate-300 border rounded-md w-1/6 h-max shadow-2xl" v-for="(item, ix) in itensCard" v-bind:key="ix">
         <div class="flex mt-7 items-end">
-          <div class="basis-1/4">
+          <div class="basis-4/4">
             <div class="flex justify-center">
               <i class="text-5xl fa-solid fa-medal text-yellow-400"></i>
             </div>
           </div>
-          <div class="basis-3/4">
-            <div class="flex justify-start">
-              <h4 class="text-3xl font-semibold">Transparência</h4>
-            </div>
-          </div>
         </div>
-
-        <div class="flex gap-4 mt-24">
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus consectetur ratione quisquam veniam, amet harum corporis placeat tempore, aliquam culpa molestiae laboriosam, impedit fugit natus dolores iusto. Dicta, reiciendis totam.
+        <div class="row-auto py-8">
+            <div class="flex justify-start">
+              <h4 class="text-3xl font-semibold">{{item.titulo}}</h4>
+            </div>
+        </div>
+        <div class="flex gap-4 pb-8">
+          <p class="text-justify tracking-wide">
+            {{ item.texto }}
           </p>
         </div>
       </div>
-    </div>
 </div>
 </template>
 <style lang="scss" scoped>
